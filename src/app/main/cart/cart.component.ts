e881f6f979a597af2c9a9340e8baf3e29638f771
Angela Aguilera <213908@ust-global.com>
2022-04-04 21:49:56 -0500

    FormularioAddress

--- src/app/main/cart/cart.component.ts
+++ src/app/main/cart/cart.component.ts
@@ -1,6 +1,7 @@
 import { CartProducts } from './../../models/cartProducts';
 import { CartService } from './../../services/cart.service';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cart',
@@ -9,18 +10,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartComponent implements OnInit {
   products: CartProducts[] = [];
+  shippingForm: FormGroup;
+  total: number;
 
   constructor(
-    private cartService: CartService
+    private cartService: CartService,
+    private fb: FormBuilder
   ) { }
 
   ngOnInit(): void {
+    this.shippingForm = this.fb.group({
+      name: ['', [Validators.required,Validators.minLength(10)]],
+      city: ['', [Validators.required,Validators.minLength(2)]],
+      state: ['', [Validators.required,Validators.minLength(2)]],
+      address: ['', [Validators.required,Validators.minLength(5)]],
+      zipcode: ['', [Validators.required,Validators.minLength(8), Validators.maxLength(10)]],
+      details: ['']
+    })
     this.getProducts();
   }
 
   getProducts() {
     const userId = localStorage.getItem("userId");
-    this.cartService.getProducts(userId).subscribe(response => this.products = response);
+    this.cartService.getProducts(userId).subscribe(response => {
+      this.products = response;
+      this.total = this.products.reduce((sum,prod,) => sum + (prod.price), 0);
+    });
+    
+  }
+
+  public addAddress() {
+    if(this.shippingForm.valid) {
+      alert('The shipping information has been saved');
+    } else {
+      alert('Please enter the information required');
+    }
   }
 
 }
